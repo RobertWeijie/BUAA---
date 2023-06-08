f42768b79c20c3f8228cc0099a8e12f591b2c5b7
Weijie <101235259+RobertWeijie@users.noreply.github.com>
2023-06-08 09:43:32 +0800

    Add files via upload

---
+++ 面向对象程序设计/上机实验/第二次课上实验/Number.java
@@ -0,0 +1,31 @@
+package Factor;
+
+import java.math.BigInteger;
+
+public class Number implements Factor {
+
+    private BigInteger num;
+
+    public Number(String num) {
+        this.num = new BigInteger(num);
+    }
+
+    @Override
+    public String toString() {
+        return num.toString();
+    }
+
+    @Override
+    public Factor derive() {
+        Term term = new Term();
+        term.addFactor(this.clone());
+        /* TODO 1 */
+        return term;
+    }
+
+    @Override
+    public Factor clone() {
+        return new Number(num.toString());
+    }
+}
+
